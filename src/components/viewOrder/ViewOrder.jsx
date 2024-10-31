f8b34696e2c6683706f5b535b0618572d67afdcb
Denuka Dissanayaka <82876374+Denuka-Dissanayaka@users.noreply.github.com>
2024-10-31 15:31:59 +0530

    GI 2

--- src/components/viewOrder/ViewOrder.jsx
+++ src/components/viewOrder/ViewOrder.jsx
@@ -71,107 +71,114 @@ function ViewOrder({ openViewModal, setOpenViewModal, viewOrderId, id }) {
   }, [viewOrderId]);
 
   return (
-    <div
-      id="crud-modal"
-      tabindex="-1"
-      aria-hidden="true"
-      className={`${
-        openViewModal ? "" : "hidden"
-      } overflow-y-auto flex overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-70`}
-    >
-      <div className="relative p-4 w-full max-w-md max-h-full">
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              {`Order Info ${id}`}
-            </h3>
-            <button
-              onClick={() => setOpenViewModal(false)}
-              type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-toggle="crud-modal"
-            >
-              <svg
-                className="w-3 h-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                />
-              </svg>
-              <span className="sr-only">Close modal</span>
-            </button>
-          </div>
+    <>
+      <Invoice2
+        openInvoiceModal={openInvoiceModal}
+        setOpenInvoiceModal={setOpenInvoiceModal}
+      />
 
-          <div className="p-4">
-            <div className="m-2">
-              <p className="font-semibold text-lg">Product Name :</p>
-              <p>{`${productName}`}</p>
+      <div
+        id="crud-modal"
+        tabindex="-1"
+        aria-hidden="true"
+        className={`${
+          openViewModal ? "" : "hidden"
+        } overflow-y-auto flex overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-70`}
+      >
+        <div className="relative p-4 w-full max-w-md max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                {`Order Info ${id}`}
+              </h3>
+              <button
+                onClick={() => setOpenViewModal(false)}
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-toggle="crud-modal"
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
             </div>
-            <div className="m-2">
-              <p className="font-semibold text-lg">Order ID :</p>
-              <p>{`${uuid}`}</p>
-            </div>
-            <div className="m-2">
-              <p className="font-semibold text-lg">Customer Name :</p>
-              <p>{`${customer}`}</p>
-            </div>
-            <div className="m-2">
-              <p className="font-semibold text-lg">Customer Email :</p>
-              <p>{`${customerEmail}`}</p>
-            </div>
-            <div className="m-2">
-              <p className="font-semibold text-lg">Customer Phone :</p>
-              <p>{`${customerPhone}`}</p>
-            </div>
-            <div className="m-2">
-              <p className="font-semibold text-lg">Status :</p>
-              <p>{`${status}`}</p>
-            </div>
-            <div className="m-2">
-              <p className="font-semibold text-lg">Quantity :</p>
-              <p>{`${quantity}`}</p>
-            </div>
-            <div className="m-2">
-              <p className="font-semibold text-lg">Total :</p>
-              <p>{`${total}`}</p>
-            </div>
-            <div className="m-2">
-              <p className="font-semibold text-lg">Network :</p>
-              <p>{`${network}`}</p>
-            </div>
-            <div className="m-2">
-              <p className="font-semibold text-lg">Created Date :</p>
-              <p>{`${createDate}`}</p>
-            </div>
-            <div className="m-2">
-              {/* <Link
+
+            <div className="p-4">
+              <div className="m-2">
+                <p className="font-semibold text-lg">Product Name :</p>
+                <p>{`${productName}`}</p>
+              </div>
+              <div className="m-2">
+                <p className="font-semibold text-lg">Order ID :</p>
+                <p>{`${uuid}`}</p>
+              </div>
+              <div className="m-2">
+                <p className="font-semibold text-lg">Customer Name :</p>
+                <p>{`${customer}`}</p>
+              </div>
+              <div className="m-2">
+                <p className="font-semibold text-lg">Customer Email :</p>
+                <p>{`${customerEmail}`}</p>
+              </div>
+              <div className="m-2">
+                <p className="font-semibold text-lg">Customer Phone :</p>
+                <p>{`${customerPhone}`}</p>
+              </div>
+              <div className="m-2">
+                <p className="font-semibold text-lg">Status :</p>
+                <p>{`${status}`}</p>
+              </div>
+              <div className="m-2">
+                <p className="font-semibold text-lg">Quantity :</p>
+                <p>{`${quantity}`}</p>
+              </div>
+              <div className="m-2">
+                <p className="font-semibold text-lg">Total :</p>
+                <p>{`${total}`}</p>
+              </div>
+              <div className="m-2">
+                <p className="font-semibold text-lg">Network :</p>
+                <p>{`${network}`}</p>
+              </div>
+              <div className="m-2">
+                <p className="font-semibold text-lg">Created Date :</p>
+                <p>{`${createDate}`}</p>
+              </div>
+              <div className="m-2">
+                {/* <Link
                 to={`/invoice/${viewOrderId}`}
                 target="_blank"
                 className="inline-block bg-amber-500 p-4 mt-2 font-semibold"
               >
                 Generate Invoice
               </Link> */}
-              <button
-                onClick={() => {
-                  setOpenInvoiceModal(true);
-                }}
-                className="inline-block bg-amber-500 p-4 mt-2 font-semibold"
-              >
-                Generate Invoice
-              </button>
+                <button
+                  onClick={() => {
+                    setOpenInvoiceModal(true);
+                  }}
+                  className="inline-block bg-amber-500 p-4 mt-2 font-semibold"
+                >
+                  Generate Invoice
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
