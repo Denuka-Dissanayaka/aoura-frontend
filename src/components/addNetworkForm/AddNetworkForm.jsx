6bd2c9500f302a1aa9363c70ecf739f89ba2b4e8
Denuka Dissanayaka <82876374+Denuka-Dissanayaka@users.noreply.github.com>
2024-10-31 21:38:50 +0530

    network type

--- src/components/addNetworkForm/AddNetworkForm.jsx
+++ src/components/addNetworkForm/AddNetworkForm.jsx
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 
 function AddNetworkForm({ openModal, setOpenModal, getNetworksFunc }) {
   const [networkName, setNetworkName] = useState("");
+  const [type, setType] = useState("");
 
   const [msg, setMsg] = useState("");
   const { user } = useSelector((state) => state.auth);
@@ -18,6 +19,7 @@ function AddNetworkForm({ openModal, setOpenModal, getNetworksFunc }) {
         "https://aoura-backend-production.up.railway.app/api/v1/networks",
         {
           networkName: networkName,
+          type: type,
         },
         {
           headers: {
@@ -101,6 +103,27 @@ function AddNetworkForm({ openModal, setOpenModal, getNetworksFunc }) {
                   placeholder="Type product name"
                   required
                 />
+              </div>
+              <div className="col-span-2 ">
+                <label
+                  for="productType"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Type
+                </label>
+                <select
+                  id="productType"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  value={type}
+                  onChange={(e) => {
+                    setType(e.target.value);
+                  }}
+                >
+                  <option selected="">Select Type</option>
+
+                  <option value="physical">Physical</option>
+                  <option value="virtual">Virtual</option>
+                </select>
               </div>
             </div>
             <button
