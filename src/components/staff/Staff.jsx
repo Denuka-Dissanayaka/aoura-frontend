5e31255bb98ed5c7cf4bf93351d4a65b2bb7573c
Denuka Dissanayaka <82876374+Denuka-Dissanayaka@users.noreply.github.com>
2024-11-27 21:12:40 +0530

    getStaffsBasedOnNetwork update 3

--- src/components/staff/Staff.jsx
+++ src/components/staff/Staff.jsx
@@ -24,7 +24,7 @@ function Staff() {
 
   const [page, setPage] = useState(0);
   const [pageWhenNetworkSelected, setPageWhenNetworkSelected] = useState(0);
-  const [limit, setLimit] = useState(2);
+  const [limit, setLimit] = useState(8);
   const [pages, setPages] = useState(0);
   const [rows, setRows] = useState(0);
 
