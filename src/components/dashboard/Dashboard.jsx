7255aee4722f6447bcc97489800c06d4a57719c4
Denuka Dissanayaka <82876374+Denuka-Dissanayaka@users.noreply.github.com>
2024-12-03 00:10:51 +0530

    pie chart 5

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -32,27 +32,13 @@ ChartJS.register(
 );
 
 const data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  labels: ["Red", "Green"],
   datasets: [
     {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
+      label: "# ",
+      data: [19, 5],
+      backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(75, 192, 192, 0.2)"],
+      borderColor: ["rgba(255, 99, 132, 1)", "rgba(75, 192, 192, 1)"],
       borderWidth: 1,
     },
   ],
@@ -249,9 +235,9 @@ function Dashboard() {
           <Bar data={dataBar1} />
         </div>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-3">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5">
         <div className="bg-white p-4 dark:bg-gray-800 rounded-lg shadow-md">
-          <h3 className="text-lg font-semibold mb-4">Sales Data</h3>
+          <h3 className="text-lg font-semibold mb-4">Orders Data</h3>
           <Pie data={data} />
         </div>
       </div>
