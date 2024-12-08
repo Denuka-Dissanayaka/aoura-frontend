1ef95b3d006a8a47dd2b9d3757f63282230e767b
Denuka Dissanayaka <82876374+Denuka-Dissanayaka@users.noreply.github.com>
2024-12-08 21:10:19 +0530

    get staff by name 2

--- src/components/staff/Staff.jsx
+++ src/components/staff/Staff.jsx
@@ -205,7 +205,9 @@ function Staff() {
                   setSearchByName(e.target.value);
                 }}
                 id="earchByName"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                className={`bg-gray-50 border ${
+                  !network && "hidden"
+                } border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500`}
                 placeholder="Search By Name"
                 disabled={!network}
               />
