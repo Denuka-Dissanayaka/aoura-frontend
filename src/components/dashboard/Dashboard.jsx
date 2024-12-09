57d574a0ea28a38239f1f9efc3b2fff85c7f948b
Denuka Dissanayaka <82876374+Denuka-Dissanayaka@users.noreply.github.com>
2024-12-09 13:39:45 +0530

    dashboard update 3.2

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -226,7 +226,7 @@ function Dashboard() {
       </div>
 
       <div className="grid grid-cols-1 gap-4">
-        <div className="bg-white p-4 dark:bg-gray-800 rounded-lg shadow-md">
+        <div className="bg-white p-4 dark:bg-gray-800 rounded-lg shadow-md h-80">
           <h3 className="text-lg font-semibold mb-4">Sales Data</h3>
           <Line data={dataLine1} />
         </div>
