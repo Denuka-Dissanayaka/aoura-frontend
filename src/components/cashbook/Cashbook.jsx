5816b3e1673755598a7ec2bf8c670f894b73d1d8
Denuka Dissanayaka <82876374+Denuka-Dissanayaka@users.noreply.github.com>
2025-02-02 11:58:51 +0530

    cashbook page

--- src/components/cashbook/Cashbook.jsx
+++ src/components/cashbook/Cashbook.jsx
@@ -11,6 +11,27 @@ function Cashbook() {
   return (
     <div className="grow p-8">
       <h2 className="text-2xl mb-4">Cashbook</h2>
+
+      <div>
+        <div className="grid grid-cols-4 gap-2">
+          <div className="p-2">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              iusto dolore rerum corporis fugiat consequatur quod molestias,
+              earum quos temporibus?
+            </p>
+            <div>
+              <button className="bg-dark-purple dark:bg-gray-800 hover:bg-dark-purple-[300] text-white font-bold py-2 px-4 rounded">
+                {" "}
+                View Details
+              </button>
+              <button className="bg-dark-purple dark:bg-gray-800 hover:bg-dark-purple-[300] text-white font-bold py-2 px-4 rounded">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
