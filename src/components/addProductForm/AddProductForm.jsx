b9ed5401f2fc97753163e1f6e7b91a353a0b9cf2
Denuka Dissanayaka <82876374+Denuka-Dissanayaka@users.noreply.github.com>
2025-02-16 17:57:28 +0530

    image

--- src/components/addProductForm/AddProductForm.jsx
+++ src/components/addProductForm/AddProductForm.jsx
@@ -14,6 +14,8 @@ function AddProductForm({ openModal, setOpenModal, getProductsFunc }) {
   const [networks, setNetworks] = useState([]);
   const [network, setNetwork] = useState([]);
   const [msg, setMsg] = useState("");
+  const [file, setFile] = useState("");
+  const [preview, setPreview] = useState("");
   const { user } = useSelector((state) => state.auth);
   const navigate = useNavigate();
 
@@ -52,6 +54,12 @@ function AddProductForm({ openModal, setOpenModal, getProductsFunc }) {
       setNetwork(user.networkId);
     }
   }, [user]);
+
+  const loadImage = (e) => {
+    const image = e.target.files[0];
+    setFile(image);
+    setPreview(URL.createObjectURL(image));
+  };
 
   const saveProduct = async (e) => {
     e.preventDefault();
@@ -243,6 +251,29 @@ function AddProductForm({ openModal, setOpenModal, getProductsFunc }) {
                   value={productQuantity}
                   onChange={(e) => {
                     setProductQuantity(e.target.value);
+                  }}
+                  id="quantity"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="$Quantity"
+                />
+              </div>
+
+              <div
+                className={`col-span-2 sm:col-span-1 ${
+                  type === "product" ? "" : "hidden"
+                }`}
+              >
+                <label
+                  for="image"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Image
+                </label>
+                <input
+                  type="file"
+                  name="image"
+                  onChange={(e) => {
+                    loadImage(e);
                   }}
                   id="quantity"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
